48a694278a48bb67ad8aae49a20c0ef423377c1b
Brian Jubelirer <BJubelirer@dzynetech.com>
2021-06-15 10:25:18 -0400

    automated created_at and updated_at fields

--- schema.sql
+++ schema.sql
@@ -17,14 +17,29 @@ CREATE TABLE public.permits(
     city VARCHAR(255),
     state VARCHAR(255),
     source_id int,
-    updated_at date DEFAULT now(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     is_construction boolean,
     CONSTRAINT fk_source
         FOREIGN KEY(source_id)
             REFERENCES sources(id)
 );
 SELECT AddGeometryColumn ('public','permits','location',4326,'POINT',2);
+-- automatic updated_at timestamp
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON public.permits 
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- end automatic updated_at timestamp
 
 -- example of adding data using WKT
 INSERT INTO
