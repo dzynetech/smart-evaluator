e4955a8e90c41a90d555658853e6cc7b907b1dfa
Brian Jubelirer <BJubelirer@dzynetech.com>
2021-06-30 12:08:29 -0400

    mutations working

---
+++ frontend/index.js
@@ -0,0 +1,121 @@
+function next_page() {
+  page++;
+  document.getElementById("pagecount").innerHTML = page + 1;
+  clear_permits();
+  get_list();
+}
+function prev_page() {
+  if (page <= 0) {
+    return;
+  }
+  page--;
+  document.getElementById("pagecount").innerHTML = page + 1;
+  clear_permits();
+  get_list();
+}
+var page = 0;
+var permits_per_page = 10;
+
+function clear_permits() {
+  let div = document.getElementById("permits");
+  div.innerHTML = "";
+}
+
+async function classify(id) {
+  let mutation = `
+	mutation mark_${id}_as_construction {
+		updatePermitById(input: { permitPatch: { isConstruction: true }, id: 8051 }) {
+			clientMutationId
+			permit {
+				id
+				cost
+				city
+				sqft
+				state
+				street
+				streetNumber
+				sourceBySourceId {
+					name
+				}
+			}
+		}
+	}	
+	`;
+  var response = await fetch("/graphql", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: mutation,
+      operationName: `mark_${id}_as_construction`,
+    }),
+  });
+  if (!response.ok) {
+    console.log(response.status);
+    console.log(response.statusText);
+    var data = await response.json();
+    console.log(data);
+  }
+  var elements = document.getElementById(id).children;
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].tagName == "BUTTON") {
+      elements[i].remove();
+    }
+  }
+}
+
+async function get_list() {
+  let query = `
+query MyQuery {
+  allPermits(
+    first: ${permits_per_page}
+	offset: ${page}
+    orderBy: COST_DESC
+    filter: {isConstruction: {isNull: true}}
+  ) {
+    edges {
+      node {
+        id
+        cost
+        city
+        sqft
+        state
+        street
+        streetNumber
+        sourceBySourceId {
+          name
+        }
+      }
+    }
+  }
+}
+`;
+  var response = await fetch("/graphql", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: query, operationName: "MyQuery" }),
+  });
+  var data = await response.json();
+  console.log(data);
+  data.data.allPermits.edges.forEach((permit) => {
+    let div = document.getElementById("permits");
+    let container = document.createElement("div");
+    container.id = permit.node.id;
+    let code = document.createElement("code");
+    let button = document.createElement("button");
+    button.innerText = `classify ${permit.node.id}`;
+    button.onclick = () => {
+      classify(permit.node.id);
+    };
+    code.innerText = JSON.stringify(permit.node);
+    div.appendChild(container);
+    container.appendChild(code);
+    container.appendChild(button);
+    console.log(permit.node);
+  });
+}
+
+get_list();
