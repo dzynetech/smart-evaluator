a0ea13f86552b3c31d13a43ec740acc8b96ac901
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-08-31 10:39:43 -0400

    nginx rev proxy

--- testcurl.sh
+++
@@ -1,5 +0,0 @@
-curl -g --user admin:admin \
-	-X POST \
-	-H "Content-Type: application/json" \
-	-d '{"query":"query MyQuery {\n  query {\n    permits(first: 1) {\n      edges {\n        node {\n          id\n        }\n      }\n    }\n  }\n}\n","operationName":"MyQuery"}' \
-	http://smart.dzynetech.com:4401/graphql
