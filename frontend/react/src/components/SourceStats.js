7140f86d1d80984897245720a821f6d35fb5f9fc
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-09-14 17:17:16 -0400

    match stats colors to permits, dupicate color

--- frontend/react/src/components/SourceStats.js
+++ frontend/react/src/components/SourceStats.js
@@ -1,5 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
 import { Pie } from "react-chartjs-2";
+import { altColorMap, borderColorMap } from "../utils/Colors";
 
 const TOTAL_QUERY = gql`
   query TotalPermits($sourceId: Int) {
@@ -72,19 +73,19 @@ function SourceStats(props) {
     datasets: [
       {
         label: "Rainfall",
-        backgroundColor: [
-          "#B21F00",
-          "#C9DE00",
-          "#2FDE00",
-          "#00A6B4",
-          "#6800B4",
-        ],
         hoverBackgroundColor: [
-          "#501800",
-          "#4B5000",
-          "#175000",
-          "#003350",
-          "#35014F",
+          altColorMap.UNCLASSIFIED,
+          altColorMap.CONSTRUCTION,
+          altColorMap.NOT_CONSTRUCTION,
+          altColorMap.POSSIBLE_CONSTRUCTION,
+          altColorMap.DUPLICATE,
+        ],
+        backgroundColor: [
+          borderColorMap.UNCLASSIFIED,
+          borderColorMap.CONSTRUCTION,
+          borderColorMap.NOT_CONSTRUCTION,
+          borderColorMap.POSSIBLE_CONSTRUCTION,
+          borderColorMap.DUPLICATE,
         ],
         data: [
           unclassified.data.permits.totalCount,
