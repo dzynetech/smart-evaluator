4c1762013722c7f24158d36652fe940edc5549b6
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-09-29 11:41:34 -0400

    start on jwt react login

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -1,24 +1,38 @@
 import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
+import { setContext } from "@apollo/client/link/context";
 import Stats from "./components/Stats.js";
 import Permits from "./components/Permits.js";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { createContext } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 var graphql_url = process.env.REACT_APP_GRAPHQL_URL;
 if (!graphql_url) {
   graphql_url = "http://" + window.location.host + "/graphql";
 }
-const client = new ApolloClient({
-  uri: graphql_url,
-  cache: new InMemoryCache(),
-});
 
 export const permitContext = createContext(null);
 
 function App() {
+  const [jwt, setJwt] = useLocalStorage("jwt", null);
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: jwt ? `Bearer ${jwt}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    uri: graphql_url,
+    cache: new InMemoryCache(),
+    link: authLink,
+  });
+
   return (
     <ApolloProvider client={client}>
       <Router>
