ef2fe3b60a9242c568529b5cd1988a47faf287f6
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-10-04 20:44:34 +0000

    fix migrations

--- migrations/sqls/20211004203742-permits-row-security-up.sql
+++ migrations/sqls/20211004203742-permits-row-security-up.sql
@@ -1,3 +1,5 @@
+
+DROP POLICY select_permit ON smart.permits;
 grant execute on function smart.can_view_permit(integer) to smart_user;
 
 create policy select_permit on smart.permits for select to smart_user USING 
