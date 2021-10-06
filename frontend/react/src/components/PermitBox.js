bddfeb14f9e81d22c3643a1a4d1cb081cb0dc356
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-10-06 16:26:55 -0400

    urbanscape video

--- frontend/react/src/components/PermitBox.js
+++ frontend/react/src/components/PermitBox.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import PermitData from "./PermitData";
 import PermitNote from "./PermitNote.js";
 import PermitButtons from "./PermitButtons.js";
 import { colorMap, borderColorMap } from "../utils/Colors.js";
 
 import "./PermitBox.css";
+import UrbanscapeVideos from "./UrbanscapeVideos";
 
 function PermitBox(props) {
+  const [showUrbanscape, setShowUrbanscape] = useState(false);
+  const videoRef = useRef(null);
+
   const image_dir = "/data/";
   const mp4_filename = image_dir + props.permit.imageUrl + ".mp4";
   const borderColor = borderColorMap[props.permit.classification];
@@ -16,92 +20,108 @@ function PermitBox(props) {
     <>
       <div className="row">
         <div
-          className="permitBox"
+          className="permit-box"
           style={{ borderColor: borderColor, backgroundColor: backgroundColor }}
           id={props.permit.id}
         >
-          <div className="video">
-            <video autoPlay loop muted controls width={512}>
-              <source src={mp4_filename} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-          <div className="permit-title">
-            <b style={{ fontSize: "23px" }}>{props.permit.name}</b>
-          </div>
-          <div className="permit-info">
-            <table>
-              <tbody>
-                {props.permit.cost > 0 && (
+          <div className="permit-grid">
+            <div className="video">
+              <video ref={videoRef} autoPlay loop muted controls width={512}>
+                <source src={mp4_filename} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <div className="permit-title">
+              <b style={{ fontSize: "23px" }}>{props.permit.name}</b>
+            </div>
+            <div className="permit-info">
+              <table>
+                <tbody>
+                  {props.permit.cost > 0 && (
+                    <tr>
+                      <td>Cost: </td>
+                      <td>
+                        <b className="f18">
+                          ${props.permit.cost.toLocaleString()}
+                        </b>
+                      </td>
+                    </tr>
+                  )}
+                  {props.permit.sqft > 0 && (
+                    <tr>
+                      <td>Sqft: </td>
+                      <td>
+                        <b className="f18">
+                          {props.permit.sqft.toLocaleString()}
+                        </b>
+                      </td>
+                    </tr>
+                  )}
                   <tr>
-                    <td>Cost: </td>
+                    <td>Permit Date:&nbsp;&nbsp;&nbsp;</td>
+                    <td>{props.permit.issueDate}</td>
+                  </tr>
+                  <tr>
+                    <td>Lat/Long:</td>
                     <td>
-                      <b className="f18">
-                        ${props.permit.cost.toLocaleString()}
-                      </b>
+                      ({props.permit.location.y},{props.permit.location.x})
                     </td>
                   </tr>
-                )}
-                {props.permit.sqft > 0 && (
-                  <tr>
-                    <td>Sqft: </td>
-                    <td>
-                      <b className="f18">
-                        {props.permit.sqft.toLocaleString()}
-                      </b>
-                    </td>
-                  </tr>
-                )}
-                <tr>
-                  <td>Permit Date:&nbsp;&nbsp;&nbsp;</td>
-                  <td>{props.permit.issueDate}</td>
-                </tr>
-                <tr>
-                  <td>Lat/Long:</td>
-                  <td>
-                    ({props.permit.location.y},{props.permit.location.x})
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+                </tbody>
+              </table>
+            </div>
+            <div className="permit-actions">
+              <p>Construction?</p>
+              <PermitButtons
+                permit={props.permit}
+                nextPermit={props.nextPermit}
+                setActivePermit={props.setActivePermit}
+              />
+              <PermitNote permit={props.permit} />
+            </div>
+            <div className="permit-data">
+              <PermitData
+                permit={props.permit}
+                border={borderColor}
+                backgroundColor={backgroundColor}
+              />
+            </div>
+            <div className="permit-sidebar text-center">
+              <h3
+                style={{ marginTop: "1rem" }}
+                onClick={() => {
+                  if (props.setActivePermit) {
+                    props.setActivePermit({
+                      ...props.permit,
+                      time: Date.now(),
+                    });
+                  }
+                }}
+              >
+                <i className="bi bi-geo-alt"></i>
+              </h3>
+              <div>
+                {["2016", "2017", "2018", "2019", "2020", "2021"].map((y) => (
+                  <div key={y}>
+                    <a
+                      href={
+                        image_dir + props.permit.id + " " + y + "-07-01.kml"
+                      }
+                    >
+                      {y}
+                    </a>
+                    <br />
+                  </div>
+                ))}
+              </div>
+              <button onClick={() => setShowUrbanscape((x) => !x)}>
+                Videos
+              </button>
+            </div>
           </div>
-          <div className="permit-actions">
-            <p>Construction?</p>
-            <PermitButtons
-              permit={props.permit}
-              nextPermit={props.nextPermit}
-              setActivePermit={props.setActivePermit}
-            />
-            <PermitNote permit={props.permit} />
-          </div>
-          <div className="permit-data">
-            <PermitData
-              permit={props.permit}
-              border={borderColor}
-              backgroundColor={backgroundColor}
-            />
-          </div>
-          <div className="permit-sidebar text-center">
-            <br />
-            <h3
-              onClick={() => {
-                if (props.setActivePermit) {
-                  props.setActivePermit({ ...props.permit, time: Date.now() });
-                }
-              }}
-            >
-              <i className="bi bi-geo-alt"></i>
-            </h3>
-            <br />
-            {["2016", "2017", "2018", "2019", "2020", "2021"].map((y) => (
-              <React.Fragment key={y}>
-                <a href={image_dir + props.permit.id + " " + y + "-07-01.kml"}>
-                  {y}
-                </a>
-                <br />
-              </React.Fragment>
-            ))}
-          </div>
+          {showUrbanscape && (
+            <UrbanscapeVideos id={props.permit.id} masterVideoRef={videoRef} />
+          )}
         </div>
       </div>
     </>
---
+++ frontend/react/src/components/UrbanscapeVideos.js
@@ -0,0 +1,89 @@
+import { useEffect, useRef } from "react";
+
+const video_suffixes = [
+  "_Depth_PNG",
+  "_Weighted_Depth",
+  "_Depth_BLD",
+  "_Overlay_BLD",
+  "_Mask_BLD",
+];
+const prefix = "/data/urbanscape_videos/";
+
+function UrbanscapeVideos(props) {
+  return (
+    <div className="video-container">
+      {video_suffixes.map((suffix) => (
+        <div>
+          <UrbanscapeVideo
+            id={props.id}
+            masterVideoRef={props.masterVideoRef}
+            suffix={suffix}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function UrbanscapeVideo(props) {
+  const videoRef = useRef(null);
+
+  function isPlaying(vid) {
+    return !!(
+      vid.currentTime > 0 &&
+      !vid.paused &&
+      !vid.ended &&
+      vid.readyState > 2
+    );
+  }
+
+  function onPlay() {
+    videoRef.current.play();
+  }
+  function onPause() {
+    videoRef.current.pause();
+  }
+  function onSeek() {
+    if (videoRef.current) {
+      videoRef.current.currentTime = props.masterVideoRef.current.currentTime;
+    }
+  }
+
+  useEffect(() => {
+    if (!props.masterVideoRef.current || !videoRef.current) {
+      return;
+    }
+    if (isPlaying(props.masterVideoRef.current)) {
+      videoRef.current.play();
+    }
+    videoRef.current.currentTime = props.masterVideoRef.current.currentTime;
+    props.masterVideoRef.current.addEventListener("play", onPlay);
+    props.masterVideoRef.current.addEventListener("pause", onPause);
+    props.masterVideoRef.current.addEventListener("seeking", onSeek);
+    props.masterVideoRef.current.addEventListener("seeked", onSeek);
+
+    return () => {
+      // //release video memory
+      // videoRef.current.pause();
+      // videoRef.current.src = "";
+      // videoRef.current.load();
+
+      // Remove event listeners
+      props.masterVideoRef.current.removeEventListener("play", onPlay);
+      props.masterVideoRef.current.removeEventListener("pause", onPause);
+      props.masterVideoRef.current.removeEventListener("seeking", onSeek);
+      props.masterVideoRef.current.removeEventListener("seeked", onSeek);
+    };
+  }, [videoRef]);
+
+  return (
+    <video ref={videoRef} width="540" loop muted controls>
+      <source
+        src={prefix + props.id + props.suffix + ".mp4"}
+        type="video/mp4"
+      />
+    </video>
+  );
+}
+
+export default UrbanscapeVideos;
