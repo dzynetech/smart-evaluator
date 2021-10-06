73709f20a18cc655c6610fa592d5ca49e1a9a749
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-10-06 10:05:56 -0400

    marker style looks better on all layers

--- frontend/react/src/components/Map.js
+++ frontend/react/src/components/Map.js
@@ -68,7 +68,6 @@ function Map(props) {
     Object.assign(queryVars, props.filterVars);
     queryVars.numPerPage = 9999;
     queryVars.offset = 0;
-    queryVars.hasBounds = router_location.pathname == "/imerit_sites";
     getPermits({ variables: queryVars });
   }, [props.filterVars]);
 
