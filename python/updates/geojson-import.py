be5d5087ae58876dbb8f9173235e63b32509f60a
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-10-26 17:10:18 -0400

    no null street_number on geojson import

--- python/updates/geojson-import.py
+++ python/updates/geojson-import.py
@@ -38,10 +38,10 @@ def main():
             area = props['area']
         except:
             area = 0
-        sql = "INSERT INTO smart.permits (bounds, permit_data,source_id,import_id,cost,sqft,city,state,street,zip,name,notes) "
+        sql = "INSERT INTO smart.permits (bounds, permit_data,source_id,import_id,cost,sqft,city,state,street,street_number,zip,name,notes) "
         sql += "VALUES (ST_GeomFromGeoJSON(%s),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) RETURNING id;"
         cursor.execute(sql, (geojson, permit_data,
-                       source_id, import_id, 0, area * meters_to_sqft, "", "", "", "", props['site_id'], ""))
+                       source_id, import_id, 0, area * meters_to_sqft, "", "", "", "","", props['site_id'], ""))
         id = cursor.fetchone()[0]
         print(id)
         sql = "UPDATE smart.permits SET location = ST_Centroid(bounds) where id=%s"
