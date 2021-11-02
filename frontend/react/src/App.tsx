42ecdfb12c846ebc09a66b21822e5bab0e065291
Brian Jubelirer <bjubelirer@dzynetech.com>
2021-11-02 15:17:12 +0000

    add ssl to staging

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -16,7 +16,7 @@ import useLocalStorage from "./hooks/useLocalStorage";
 
 var graphql_url = process.env.REACT_APP_GRAPHQL_URL;
 if (!graphql_url) {
-  graphql_url = "http://" + window.location.host + "/graphql";
+  graphql_url = "https://" + window.location.host + "/graphql";
 }
 
 function App() {
