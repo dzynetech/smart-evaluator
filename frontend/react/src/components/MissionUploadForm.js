969784a814c75c8a201dd3d78c9e2f2b48c60b8c
Brian Jubelirer <bjubelirer@dzynetech.com>
2022-10-18 11:25:57 -0400

    fixup

--- frontend/react/src/components/MissionUploadForm.js
+++ frontend/react/src/components/MissionUploadForm.js
@@ -90,7 +90,7 @@ export default function MissionUploadForm({ col }) {
 				return
 			}
 			if (result.file) {
-				sendMissionToASM(result.file)
+				// sendMissionToASM(result.file)
 			}
 		}
 		catch (error) {
