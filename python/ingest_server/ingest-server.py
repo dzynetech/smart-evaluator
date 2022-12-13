ca50bbc921ab05957208d840ef22b9e298144b35
Brian Jubelirer <bjubelirer@dzynetech.com>
2022-12-13 15:11:09 -0500

    first attempt at fallover to new import json

--- python/ingest_server/ingest-server.py
+++ python/ingest_server/ingest-server.py
@@ -85,7 +85,10 @@ def ingest(data,source,user_id):
         import_id = uuid.uuid4().hex[:16]
         print("Import ID:",import_id)
 
-        sites = data['results']
+        try:
+            sites = data['results']
+        except (KeyError,TypeError):
+            return new_ingest(data,import_id,source,user_id)
 
         sql = f"INSERT INTO smart.sources(name) VALUES(%s) RETURNING id"
         cursor.execute(sql,(source,))
@@ -118,6 +121,61 @@ def ingest(data,source,user_id):
         # cursor.close()
         # connection.close()
         return len(sites)
+    except psycopg2.DatabaseError as e:
+        cursor.execute("ROLLBACK")
+        raise e
+
+def is_site(x):
+    try:
+        return x['properties']['type'] == "site"
+    except:
+        return False
+
+def new_ingest(data,import_id,source,user_id):
+    try:
+        print("Failing over to new ingest")
+
+        sql = f"INSERT INTO smart.sources(name) VALUES(%s) RETURNING id"
+        cursor.execute(sql,(source,))
+        source_id = cursor.fetchone()[0]
+
+        if user_id is not None:
+            # give this user access to that source
+            sql = f"INSERT INTO smart_private.users_sources (user_id,source_id) VALUES(%s,%s)"
+            cursor.execute(sql,(user_id,source_id))
+
+        for feature_site in data:
+            features = feature_site['features']
+            site = None
+            for f in features:
+                if is_site(f):
+                    site = f
+                    break
+
+            try:
+                site_id = site['properties']['site_id']
+            except:
+                site_id = "???"
+            permit_data = json.dumps(site)
+            area = 0
+            cost = 0
+            issue_date = site['properties']['start_date']
+            geojson = json.dumps(site['geometry'])
+            image_url ='test'
+
+            sql = "INSERT INTO smart.permits (bounds,permit_data,source_id,import_id,image_url,issue_date,cost,sqft,city,state,street,street_number,zip,name,notes) "
+            sql += "VALUES (ST_Multi(ST_GeomFromGeoJSON(%s)),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) RETURNING id;"
+            cursor.execute(sql, (geojson, permit_data,
+                        source_id, import_id, image_url, issue_date,cost, area * meters_to_sqft, "", "", "", "", "", site_id, ""))
+            id = cursor.fetchone()[0]
+            print(id)
+            sql = "UPDATE smart.permits SET location = ST_Centroid(bounds) where id=%s"
+            cursor.execute(sql, (id,))
+
+        connection.commit()
+        # cursor.close()
+        # connection.close()
+        return len(data)
     except psycopg2.DatabaseError as e:
         cursor.execute("ROLLBACK")
         raise e
