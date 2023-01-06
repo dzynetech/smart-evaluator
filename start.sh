70f2ac786bd9fbf814d2f235a3c6b8d79f67dd65
Brian Jubelirer <bjubelirer@dzynetech.com>
2023-01-06 14:34:32 -0500

    update helper scripts for new yamls

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-[ "$#" -ne 1 ] && (echo "Specify d,s, or p for dev,staging or production environment" && exit 1)
+[ "$#" -ne 1 ] && (echo "Specify d,p, or a for dev, production, or aws environment" && exit 1)
 
-[[ "$1" = "p" ]] && (docker-compose -f production.yml up --build -d && exit)
-[[ "$1" = "s" ]] && (docker-compose -f staging.yml up --build -d && exit) 
-[[ "$1" = "d" ]] && (docker-compose up --build -d && exit)
+[[ "$1" = "a" ]] && docker-compose -f aws.yml up -d
+[[ "$1" = "p" ]] && docker-compose -f production.yml up --build -d
+[[ "$1" = "d" ]] && docker-compose up --build -d
--- stop.sh
+++ stop.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
-[ "$#" -ne 1 ] && (echo "Specify d,s, or p for dev,staging or production environment" && exit 1)
+[ "$#" -ne 1 ] && (echo "Specify d,p, or a for dev, production, or aws environment" && exit 1)
 
-[[ "$1" = "p" ]] && (docker-compose -f production.yml down && exit )
-[[ "$1" = "s" ]] && (docker-compose -f staging.yml down && exit )
-[[ "$1" = "d" ]] && (docker-compose down && exit )
-
+[[ "$1" = "a" ]] && (docker-compose -f aws.yml down
+[[ "$1" = "p" ]] && (docker-compose -f production.yml down
+[[ "$1" = "d" ]] && (docker-compose down
